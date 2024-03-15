8c260dcf7284c83dadd7213f1afc2a29a8eded19
ashishmisal23 <ashishmisal23@gmail.com>
2024-03-15 16:19:51 +0530

    About page height managed

--- src/componants/About.js
+++ src/componants/About.js
@@ -5,8 +5,8 @@ const About = () => {
 
 
   return (
-    <div className='container'>
-      <h2 className='text-center mt-5 p-2'>About Todos List</h2>
+    <div className='container min-vh-100'>
+      <h2 className='text-center mt-5 p-2 h-100'>About Todos List</h2>
       <p></p>
     </div>
   )
